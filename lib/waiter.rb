21b23ce0d1f9da9b716916f48852898886d0b7f1
Zach  Smith <zdsmith001@gmail.com>
2019-08-30 15:14:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
+  attr_accessor :name, :experience
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def all?
+    @@all
+  end
 
 end
