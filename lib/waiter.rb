fec871414b3c6c50b468c9ea4960073a6def217f
Zach  Smith <zdsmith001@gmail.com>
2019-08-30 15:25:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,11 +16,12 @@ class Waiter
     @@all
   end
 
-  def new_meal(customer, total, tip)
+  def new_meal(customer, total, tip=0)
     new_meal = Meal.new(self, customer, total, tip)
   end
 
   def meals
+    Meal.all.select {|meal|meal.waiter == self}
   end
 
   def best_tipper
